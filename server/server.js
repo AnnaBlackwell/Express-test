513aa04901f9b30999fce5665cd51b5d43551f44
Anna Blackwell <annablackwell@Annas-MacBook-Air.local>
2016-02-20 15:29:57 +1300

    server partially working

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var path = require('path')
-
-var app = express();
+var app = express()
+var bodyParser = require('body-parser')
+var fs = require('fs')
 
 // Define the port to run on
 app.set('port', 3000);
@@ -9,11 +10,40 @@ app.set('port', 3000);
 //Define the directory to serve as static page
 app.use(express.static(path.join(__dirname, '../static')));
 
-// Listen for requests
+//Configure to work with bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//get an instance of the express router
+var router = express.Router()
+
+//test route to check it works
+// router.get('/api/v1/fruit', function(req, res) {
+//     res.json({ message: 'hooray! welcome to our api!' });   
+// });
+
+// register routes
+// app.use('/api/v1/fruit', router);
+
+// Start the server and listen for requests
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
   console.log('Magic happens on port ' + port);
 });
 
-//routes
+//requests
+app.get('/api/v1/fruit', (req, res) => {
+    fs.readFile('../data/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data))
+  })
+})
 
+// app.get('/api/v1/vegetables', (req, res) => {
+//     fs.readFile('../data/db.json', 'utf8', (err, data) => {
+//     if (err) throw err;
+//     res.json(JSON.parse(data))
+//   })
+// })
+
+module.exports = app
